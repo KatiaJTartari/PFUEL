6735f0b32a6666735a95508d7eab85f723ec8905
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-01-29 14:56:09 -0300

    Add files via upload

---
+++ Atividades/Atividade_Aula2/Atividade1.R
@@ -0,0 +1,42 @@
+# Atividade 1 - A partir da declaração dos objetos a seguir, complemente o código 
+# com a função que exibe a classe do objeto:
+
+disciplina = "PFUEL"
+class(disciplina)
+typeof(disciplina)
+
+qtd_aulas = 95
+class(qtd_aulas)
+typeof(qtd_aulas)
+
+qtd_alunos = 10
+class(qtd_alunos)
+typeof(qtd_alunos)
+
+pratica = TRUE
+class(pratica)
+typeof(pratica)
+
+teorica = TRUE
+class(teorica)
+typeof(teorica)
+
+presencial = FALSE
+class(presencial)
+typeof(presencial)
+
+eletiva = TRUE
+class(eletiva)
+typeof(eletiva)
+
+nota_ifa = 4.5
+class(nota_ifa)
+typeof(nota_ifa)
+
+nota_aprovacao = 6
+class(nota_aprovacao)
+typeof(nota_aprovacao)
+
+raiz_quadrada = 1i
+class(raiz_quadrada)
+typeof(raiz_quadrada)
