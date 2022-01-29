9072c025ea4f9bed699324e519c32bf6eb719f81
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-01-29 14:48:16 -0300

    Add files via upload

---
+++ Atividades/Aula1.r
@@ -0,0 +1,78 @@
+# AULA 1
+
+# Slide 9:
+
+# Exibe o número 2
+2
+
+# Operação 2 + 3
+2+3
+
+# Log natural de 2
+log(2)
+
+
+# Slide 10: 
+
+# Armazena o valor 2 na variável x
+x = 2
+
+# "Printa" a variável no terminal
+x
+
+# Armazena e "printa" a variável
+(x = 3)
+
+# Armazena um texto a variável x
+x = "Hello"
+
+# "printa" o texto na variável x
+x
+
+
+#Slide 11:
+
+# Cria um vetor com 4 valores
+notas = c(10, 8, 7, 8)
+
+# "Printa" o segundo elemento do vetor
+notas[2]
+
+# "printa" do segundo ao último valor
+notas[2:4]
+
+# Cria e "printa" um vetor com base em uma sequência
+(medias = 1:10)
+
+
+# Slide 12:
+
+# Cria um vetor com 6 valores
+notas = c(10, 8, 7, 8, 6, 10)
+
+# "printa" o tamanho do vetor
+length (notas)
+
+# "printa" a média dos valores do vetor
+mean (notas)
+
+# "printa" a variância dos valores do vetor
+var(notas)
+
+
+# Slide 13:
+
+# Cria um vetor com 6 valores
+x = c(10, 10, 10, 10, 10, 10)
+
+# Cria outro vetor com 6 valores
+y = c(5, 5, 5, 5, 5, 5)
+
+# Cria uma matriz a partir dos vetor x e y
+M = cbind (x, y)
+
+# "Printa" a matriz
+M
+
+# "Printa" as dimenssões da matriz
+dim(M)
